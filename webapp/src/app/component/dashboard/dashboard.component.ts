f2a42a7409d63014b6ba5952203936fd92b1e6c7
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-03 18:45:26 -0300

    refactor: Atualizando pt_br.json

---
+++ webapp/src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,40 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
+import {PageService} from '../../shared/services/page/page-service';
+import {ButtonBuilder} from '../../shared/buiderls/button-builder';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    TranslateModule,
+  ]
+})
+export class DashboardComponent implements OnInit {
+
+  dashboard: string = ''
+  private page = inject(PageService);
+
+  ngOnInit(): void {
+    this.setup()
+  }
+
+  setup() {
+    this.page.setPage({
+      title: 'Overview',
+      subtitle: 'Dashboard',
+      showSearch: true,
+      actions: [
+        ButtonBuilder.sort({}),
+        ButtonBuilder.filter({}),
+        ButtonBuilder.calendar({}),
+      ],
+    })
+  }
+
+
+}
