e86f1be3e62767109e86f0866ccb821b80bf53bf
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-03 18:38:13 -0300

    refactor: Ajustando roteamento

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,8 +1,23 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const APP_ROUTES: Routes = [
   {
-    path: "",
-    loadChildren: () => import('./component/auth/auth-routes').then(r => r.LOGIN_ROUTES)
-  }
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./component/auth/auth-routes').then(r => r.LOGIN_ROUTES)
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./setup-page-component').then(c => c.SetupPageComponent),
+    loadChildren: () =>
+      import('./component/entity-routes').then(r => r.ENTITY_ROUTES)
+  },
+
+  {path: '**', redirectTo: 'login'}
 ];
