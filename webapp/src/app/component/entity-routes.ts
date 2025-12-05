093742446c703a7018797d49b0307362fb237562
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-05 19:38:06 -0300

    chore: adicionando onovas rotas de usuarios e roles

--- webapp/src/app/component/entity-routes.ts
+++ webapp/src/app/component/entity-routes.ts
@@ -2,11 +2,11 @@ import {Routes} from '@angular/router';
 
 export const ENTITY_ROUTES: Routes = [
   {
-    path: 'login',
-    loadChildren: () => import('./auth/auth-routes').then(r => r.LOGIN_ROUTES)
-  },
-  {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.routes').then(r => r.DASHBOARD_ROUTES)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.routes').then(r => r.USER_ROUTES)
   }
 ]
