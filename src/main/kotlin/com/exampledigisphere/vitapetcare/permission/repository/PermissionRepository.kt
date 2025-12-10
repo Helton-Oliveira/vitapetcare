76fb9b29c82b3a873b8844aea061d9c05e3ac551
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-09 21:27:19 -0300

    refactor: Removendo entidades de Role e Permission para criar nova abordagem de RBAC com agrupamento de permissoes por Role

--- src/main/kotlin/com/exampledigisphere/vitapetcare/permission/repository/PermissionRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.exampledigisphere.vitapetcare.permission.repository
-
-import com.exampledigisphere.vitapetcare.permission.domain.Permission
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PermissionRepository : JpaRepository<Permission, Long> {
-}
