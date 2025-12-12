177d919cbaca48d1e635262e1884540fa2698aa6
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-11 21:21:45 -0300

    refacor: Deixando mais claro o uso do ngx-translate no codigo

--- webapp/src/app/component/user/user.component.ts
+++ webapp/src/app/component/user/user.component.ts
@@ -1,12 +1,14 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {TranslateModule} from '@ngx-translate/core';
+import {TranslatePipe, TranslateService} from '@ngx-translate/core';
 import {PageService} from '../../shared/services/page/page-service';
 import {ButtonBuilder} from '../../shared/buiderls/button-builder';
 import {User} from '../../shared/models/user/user.model';
-import {Router} from '@angular/router';
 import {UserService} from '../../shared/services/user/user-service';
+import {UserActionsService} from './UserActionsService';
+import {RolePipe} from '../../shared/pipes/role-pipe';
+import {ActionEditorModalService} from '../../shared/services/modal/action-modal-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,38 +16,51 @@ import {UserService} from '../../shared/services/user/user-service';
   imports: [
     ReactiveFormsModule,
     CommonModule,
-    TranslateModule,
+    RolePipe,
+    TranslatePipe,
   ]
 })
 export class UserComponent implements OnInit {
   private page = inject(PageService);
-  private router = inject(Router)
+  private userActionsService = inject(UserActionsService)
   private userService = inject(UserService)
+  private translateService = inject(TranslateService);
+  private changeDetectorRef = inject(ChangeDetectorRef);
+  private actionEditorModalService = inject(ActionEditorModalService);
 
   public users: User[] = [];
 
   ngOnInit(): void {
     this.setup()
     this.userService.getAll()
-      .then(res => this.users = res.content)
+      .then(res => {
+        this.users = res.content;
+        this.changeDetectorRef.detectChanges();
+      })
   }
 
   setup() {
     this.page.setPage({
-      title: 'Users',
-      subtitle: 'usuarios',
+      title: this.translateService.instant('user.title'),
+      subtitle: this.translateService.instant('user.subTitle'),
       showSearch: true,
       actions: [
         ButtonBuilder.sort({}),
         ButtonBuilder.filter({}),
         ButtonBuilder.add({
-          title: "Novo",
-          action: () => {
-            this.router.navigate(['app', 'user-update'])
-          }
+          title: this.translateService.instant('button.new'),
+          action: () => this.userActionsService.goToNew()
         }),
       ],
-    })
+    });
+  }
+
+  async goEdit(id: string) {
+    await this.userActionsService.goToEdit(id)
+  }
+
+  disableToast(id?: string) {
+    this.actionEditorModalService.show(() => this.userService.disable(id!!), 'md');
   }
 
 
