d0d28903c951f99b433b1f745025a51708b7dfe0
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-11 21:26:24 -0300

    feat: Adicionando rota de criar/modificar usuario

--- webapp/src/app/component/user/user.routes.ts
+++ webapp/src/app/component/user/user.routes.ts
@@ -5,6 +5,16 @@ export const USER_ROUTES: Routes = [
     path: '',
     loadComponent: () =>
       import("./user.component").then(c => c.UserComponent)
+  },
+  {
+    path: 'new',
+    loadComponent: () =>
+      import("./user-update.component").then(c => c.UserUpdateComponent)
+  },
+  {
+    path: 'edit/:id',
+    loadComponent: () =>
+      import("./user-update.component").then(c => c.UserUpdateComponent)
   }
 ];
 
