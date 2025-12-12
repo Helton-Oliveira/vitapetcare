01e2c44ed2aba0d22417ae3e39f71a9bc9890940
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-11 21:27:05 -0300

    refactor: Removendo RoleRepository

--- src/main/kotlin/com/exampledigisphere/vitapetcare/roles/repository/RoleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.exampledigisphere.vitapetcare.roles.repository
-
-import com.exampledigisphere.vitapetcare.roles.domain.Role
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface RoleRepository : JpaRepository<Role, Long> {
-}
