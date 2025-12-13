97f911d57023a7479133a41505ddcf950cd67fe4
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-13 12:30:40 -0300

    feat: Criando header customizado para a pagina e adicionando logica para buascar usuario logado

--- webapp/src/app/setup-page-component.ts
+++ webapp/src/app/setup-page-component.ts
@@ -1,18 +1,42 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {PageService} from './shared/services/page/page-service';
 import {SidebarComponent} from './component/sidebar/sidebar.component';
+import {User} from './shared/models/user/user.model';
+import AuthService from './shared/services/auth/auth.service';
+import {TranslatePipe} from '@ngx-translate/core';
+import {RolePipe} from './shared/pipes/role-pipe';
 
 @Component({
   selector: 'app-setup-page',
   imports: [
     CommonModule,
     RouterOutlet,
-    SidebarComponent
+    SidebarComponent,
+    NgOptimizedImage,
+    TranslatePipe,
+    RolePipe
   ],
   template: `
-    <header>HEADER AQUI</header>
+    <header class="bg-pet-primary w-full h-20 flex items-center px-6 shadow-lg">
+
+      <div class="flex items-center w-full">
+        <img ngSrc="../assets/logo.png" class="w-[10%]" alt="Logo vita pet care" height="1024" width="1024"/>
+      </div>
+
+      <div class="flex items-center">
+        <div class="items-center justify-center">
+            <span class="text-sm text-white hidden md:inline">
+                {{ 'global.field.welcome' | translate: {name: user.name} }}
+            </span>
+          <span [ngClass]="user.role | rolePipe">{{ "global.role." + user.role | translate }}</span>
+        </div>
+
+        <div class="h-13 w-16 bg-white/30 rounded-full border-2 border-white"></div>
+      </div>
+    </header>
+
     <div class="flex h-screen w-full bg-pet-background from-cyan-50 to-cyan-200">
       <!-- SIDEBAR -->
       <app-sidebar/>
@@ -44,13 +68,15 @@ import {SidebarComponent} from './component/sidebar/sidebar.component';
               <div class="flex gap-2">
                 @for (btn of page()?.actions; track btn) {
                   <button
-                    class="btn-primary"
+                    class="btn-primary "
                     [class.flex]="true"
                     [class.items-center]="true"
                     [class.justify-center]="!btn.title"
                     [class.justify-start]="btn.title"
                     [style.background]="btn?.background"
                     (click)="btn?.action?.()"
+                    [disabled]="!btn.isActive"
+                    [ngClass]="!btn.isActive ? 'opacity-50 hover:cursor-auto' : '' "
                   >
                     @if (btn.icon) {
                       <i
@@ -81,7 +107,14 @@ import {SidebarComponent} from './component/sidebar/sidebar.component';
 
   `
 })
-export class SetupPageComponent {
+export class SetupPageComponent implements OnInit {
   private pageService = inject(PageService)
+  private authService = inject(AuthService)
   page = this.pageService.page;
+  user = new User();
+
+  ngOnInit(): void {
+    this.authService.getCurrentAccount()
+      .then(res => this.user = res);
+  }
 }
