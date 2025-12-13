72403122034bf25919fadf116163a847536bc67b
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-12 21:29:08 -0300

    feat: Adicionando condicional para dizer quando o botao estara habilitado ou nao

--- webapp/src/app/shared/buiderls/button-builder.ts
+++ webapp/src/app/shared/buiderls/button-builder.ts
@@ -4,68 +4,74 @@ import {Button} from '../models/config/button';
 @Injectable({providedIn: 'root'})
 export class ButtonBuilder {
 
-  static saveButton({title, action}: { title?: string, action?: () => void }): Button {
+  static saveButton({title, isActive, action}: { title?: string, isActive?: boolean, action?: () => void }): Button {
     return new Button(
       title,
       'fa-solid fa-floppy-disk',
       '#3182ce',
       'text-white',
       'submit',
+      isActive,
       action
     );
   }
 
-  static add({title, action}: { title?: string, action?: () => void }): Button {
+  static add({title, isActive, action}: { title?: string, isActive?: boolean, action?: () => void }): Button {
     return new Button(
       title,
       'fa-solid fa-plus',
       '#4A9327',
       '',
       'submit',
+      isActive,
       action
     );
   }
 
-  static calendar({title, action}: { title?: string, action?: () => void }): Button {
+  static calendar({title, isActive, action}: { title?: string, isActive?: boolean, action?: () => void }): Button {
     return new Button(
       title,
       'fa-solid fa-calendar',
       'orange',
       'white',
       'submit',
+      isActive,
       action
     );
   }
 
-  static filter({title, action}: { title?: string, action?: () => void }): Button {
+  static filter({title, isActive, action}: { title?: string, isActive?: boolean, action?: () => void }): Button {
     return new Button(
       title,
       'fa-solid fa-filter',
       'white',
       'gray',
       'submit',
+      isActive,
       action
     );
   }
 
-  static sort({title, action}: { title?: string, action?: () => void }): Button {
+  static sort({title, isActive, action}: { title?: string, isActive?: boolean, action?: () => void }): Button {
     return new Button(
       title,
       'fa-solid fa-sort',
       'white',
       'gray',
       'submit',
+      isActive,
       action
     );
   }
 
-  static cancel({title, action}: { title?: string, action?: () => void }): Button {
+  static cancel({title, isActive, action}: { title?: string, isActive?: boolean, action?: () => void }): Button {
     return new Button(
       title,
       'fa-solid fa-ban',
       '#727D92',
       '#FFF',
       'submit',
+      isActive,
       action
     );
   }
--- webapp/src/app/shared/models/config/button.ts
+++ webapp/src/app/shared/models/config/button.ts
@@ -6,6 +6,7 @@ export class Button {
     public background?: string,
     public fontColor?: string,
     public type?: string,
+    public isActive: boolean = true,
     public action?: () => void,
   ) {
   }
