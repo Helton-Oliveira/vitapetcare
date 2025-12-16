4706fc52d7a0bfe85966b379961da3f9a8eba6df
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-16 19:49:12 -0300

    feat: Adicionando novas permissoes aos Usuarios

--- src/main/kotlin/com/exampledigisphere/vitapetcare/roles/Role.kt
+++ src/main/kotlin/com/exampledigisphere/vitapetcare/roles/Role.kt
@@ -1,31 +1,36 @@
 package com.exampledigisphere.vitapetcare.roles
 
 import com.exampledigisphere.vitapetcare.config.security.Operation
+import com.exampledigisphere.vitapetcare.file.domain.File
 import com.exampledigisphere.vitapetcare.user.domain.User
 
 enum class Role(val permissions: Set<String>) {
   ADMIN(
-    User.UserPermissions.all()
+    User.UserPermissions.all() + File.FilePermissions.all(),
   ),
 
   MANAGER(
-    User.UserPermissions.all()
+    User.UserPermissions.all() + File.FilePermissions.all()
   ),
 
   CLIENT(
     User.UserPermissions.of(Operation.EDIT, Operation.VIEW)
+      + File.FilePermissions.of(Operation.EDIT, Operation.VIEW)
   ),
 
   RECEPTIONIST(
     User.UserPermissions.of(Operation.EDIT, Operation.VIEW)
+      + File.FilePermissions.of(Operation.EDIT, Operation.VIEW)
   ),
 
   VETERINARIAN(
     User.UserPermissions.of(Operation.EDIT, Operation.VIEW)
+      + File.FilePermissions.of(Operation.EDIT, Operation.VIEW)
   ),
 
   GROOMER(
     User.UserPermissions.of(Operation.EDIT, Operation.VIEW)
+      + File.FilePermissions.of(Operation.EDIT, Operation.VIEW)
   ),
 
   NOT_INFORMED(emptySet()),
