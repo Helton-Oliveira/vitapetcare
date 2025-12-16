227840c00e4a44728e83452f0df8292076f4edec
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-16 19:45:51 -0300

    feat: Adicionando campo de file para user

--- src/main/kotlin/com/exampledigisphere/vitapetcare/user/domain/User.kt
+++ src/main/kotlin/com/exampledigisphere/vitapetcare/user/domain/User.kt
@@ -2,16 +2,16 @@ package com.exampledigisphere.vitapetcare.user.domain
 
 import com.exampledigisphere.vitapetcare.config.root.BaseEntity
 import com.exampledigisphere.vitapetcare.config.security.Permissions
+import com.exampledigisphere.vitapetcare.file.domain.File
 import com.exampledigisphere.vitapetcare.roles.Role
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonView
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import org.hibernate.Hibernate
+import java.util.Collections.emptySet
 
 @Entity
 @Table(name = "usr_users")
@@ -37,12 +37,20 @@ class User : BaseEntity() {
   val permissions: Set<String>
     get() = role.permissions;
 
+  @field:OneToMany(mappedBy = "user", cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY)
+  @field:JsonIgnoreProperties("user")
+  @field:JsonView(Json.WithFIle::class)
+  var files: MutableSet<File?> = emptySet();
+
   fun loadRole() = Hibernate.initialize(role)
+
+  fun loadFiles() = Hibernate.initialize(files)
 
   interface Json {
     interface List;
     interface Detail : List;
-    interface All : Detail;
+    interface WithFIle : File.Json.List
+    interface All : Detail, WithFIle;
   }
 
   object UserPermissions : Permissions {
