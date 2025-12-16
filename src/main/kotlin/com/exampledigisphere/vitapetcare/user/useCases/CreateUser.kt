f13e0ef6e7474edd7b8450807eeb4545f3df4320
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2025-12-16 19:51:33 -0300

    feat: Adicionando verificacao de existencia de files ao salvar usuario

--- src/main/kotlin/com/exampledigisphere/vitapetcare/user/useCases/CreateUser.kt
+++ src/main/kotlin/com/exampledigisphere/vitapetcare/user/useCases/CreateUser.kt
@@ -1,17 +1,32 @@
 package com.exampledigisphere.vitapetcare.user.useCases
 
+import com.exampledigisphere.vitapetcare.file.useCases.SaveFile
 import com.exampledigisphere.vitapetcare.user.domain.User
 import com.exampledigisphere.vitapetcare.user.repository.UserRepository
 import jakarta.transaction.Transactional
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.util.Collections.emptySet
 
 @Service
 @Transactional
-class CreateUser(private val userRepository: UserRepository, private val passwordEncoder: PasswordEncoder) {
+class CreateUser(
+  private val userRepository: UserRepository,
+  private val passwordEncoder: PasswordEncoder,
+  private val saveFile: SaveFile
+) {
 
   fun execute(user: User): Result<User> = runCatching {
-    user.apply { password = passwordEncoder.encode(user.password) }
+    val editedFiles = user.files
+      .filterNotNull()
+      .filter { it.wasEdited() }
+      .toMutableSet()
+
+    user.apply {
+      password = passwordEncoder.encode(user.password)
+      files = emptySet()
+    }
       .let { userRepository.save(it) }
+      .also { saveFile.execute(editedFiles, it) }
   }
 }
