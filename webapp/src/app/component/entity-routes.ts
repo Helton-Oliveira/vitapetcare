cc9cc5c07fe36b97f3fe64dd8a54861bef83d1e1
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2026-02-02 23:15:54 -0300

    feat: Adicionando job as rotas da aplicacao angular

--- webapp/src/app/component/entity-routes.ts
+++ webapp/src/app/component/entity-routes.ts
@@ -8,5 +8,9 @@ export const ENTITY_ROUTES: Routes = [
   {
     path: 'user',
     loadChildren: () => import('./user/user.routes').then(r => r.USER_ROUTES)
+  },
+  {
+    path: 'job',
+    loadChildren: () => import('./job/job.routes').then(r => r.JOB_ROUTES)
   }
 ]
